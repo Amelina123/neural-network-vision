490bcf79b49834afd7e378eddf8624598e3ca579
Amelina123 <146894964+Amelina123@users.noreply.github.com>
2025-11-19 14:46:22 +0000

    tasks

--- data.py
+++ data.py
@@ -26,8 +26,16 @@ def generate_vertical_line():
     return array
 generate_vertical_line()
 
+def generate_checkboard_image():
+    array = np.zeros((8, 8), dtype=int)
+    array[1::2, ::2] = 1
+    array[::2, 1::2] = 1 
+    return array
+generate_checkboard_image()
+
+
 def generate_image_dataset(samples_per_class=100):
-    label_images = [(generate_blank, 0),(generate_horizontal_line, 1),(generate_vertical_line, 2)]
+    label_images = [(generate_blank, 0),(generate_horizontal_line, 1),(generate_vertical_line, 2), (generate_checkboard_image, 3)]
     X_list = []
     y_list = []
 
@@ -47,3 +55,23 @@ X,y = generate_image_dataset(3)
 # print(X[3].reshape(8, 8))
 # print(X[6].reshape(8, 8))
 
+def display_images(X):
+    for array in X:
+        reshape_array = array.reshape(8,8)
+        pretty_array = ["image:\n"]
+        # print (reshape_array)
+        for line in reshape_array:
+            string = "".join(str(x) for x in line)
+         
+            for i in string:
+                if i == '0':
+                    pretty_array.append(" ")
+                elif i == '1':
+                    pretty_array.append("#")
+            pretty_array.append("\n")
+        final_string = "".join(pretty_array)
+        print(final_string)
+        
+
+
+# display_images(X)
--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from tensorflow.keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from tensorflow.keras.callbacks import LambdaCallback
-from data import generate_image_dataset
+from data import generate_image_dataset, display_images
+import random
+from random import randint
+
 
 X, y = generate_image_dataset()
 RANDOM_SEED = 1242
@@ -14,10 +17,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = Sequential([
     Dense(8, activation='relu', input_shape=(64,)),
     Dense(8, activation='relu'),
-    Dense(3, activation='softmax')
+    # Dense(8, activation='relu'),
+    Dense(4, activation='softmax')
+
 ])
 
-model.compile(optimizer=Adam(),
+model.compile(optimizer=Adam(learning_rate=0.01),
     loss='sparse_categorical_crossentropy',
     metrics=['accuracy'])
 
@@ -26,3 +31,19 @@ model.fit(X_train, y_train, epochs=10)
 model.evaluate(X_test, y_test)
 
 print(model.evaluate(X_test, y_test))
+# display_images(X_test)
+
+def get_random_image():
+    digits = []
+    while len(digits) < 64:
+        digit = random.randint(0, 1)
+        digits.append(digit)
+    array = np.array(digits, dtype=int)
+    print(array)
+    
+    print(array.shape)
+    return array  
+image = get_random_image()
+
+model.predict(image)
+
